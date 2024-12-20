b5192da16aa9799b8cf1747fcd1b066f44864e5e
lord-skinner <matthew.skinner@elastic.co>
2024-12-20 13:03:30 -0600

    adding update cmd

--- cmd/root.go
+++ cmd/root.go
@@ -4,43 +4,41 @@ import (
 	"dg/style"
 	"fmt"
 	"os"
-	"runtime"
 
-	getter "github.com/hashicorp/go-getter"
 	"github.com/spf13/cobra"
 )
-
-// Define the tool version
-var version = "v0.1.0"
 
 var rootCmd = &cobra.Command{
 	Use:   "dg",
 	Short: style.Dg.Render("\ndg is a cli written in GO to help improve the DX of Analytics Engineers using dbt"),
 	Long:  style.Dg.Render("dg is a cli written in GO to help improve the DX of Analytics Engineers using dbt"),
-	RunE:  executeRoot,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if cmd.Flag("version").Changed {
+			fmt.Printf("Current Version: %s\n", version)
+			return nil
+		}
+		cmd.Help()
+		return nil
+	},
 }
 
 func init() {
 	rootCmd.PersistentFlags().BoolP("version", "v", false, "Print the version number of the CLI tool")
-	rootCmd.Flags().BoolP("update", "u", false, "Update the CLI tool to the latest version or a specified version")
-}
 
-func executeRoot(cmd *cobra.Command, args []string) error {
-	if cmd.Flag("version").Changed {
-		fmt.Printf("Current Version: %s\n", version)
-		return nil
+	updateCmd = &cobra.Command{
+		Use:   "update [version]",
+		Short: "Update the CLI tool to the latest or specified version",
+		Args:  cobra.MaximumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var updateVersion string
+			if len(args) > 0 {
+				updateVersion = args[0]
+			}
+			return runUpdate(updateVersion)
+		},
 	}
 
-	if cmd.Flag("update").Changed {
-		var updateVersion string
-		if len(args) > 0 {
-			updateVersion = args[0]
-		}
-		return runUpdate(updateVersion)
-	}
-
-	cmd.Help()
-	return nil
+	rootCmd.AddCommand(updateCmd)
 }
 
 func Execute() {
@@ -49,41 +47,4 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func runUpdate(version string) error {
-	versionParam := "latest/download/dg"
-
-	if version != "" {
-		versionParam = fmt.Sprintf("download/%s/dg", version)
-	}
-
-	url := fmt.Sprintf("https://github.com/cognite-analytics/dbt-go/releases/%s", versionParam)
-
-	fmt.Printf("Updating CLI tool from %s...\n", url)
-
-	tempFile := "/tmp/dg"
-	if runtime.GOOS == "windows" {
-		tempFile = "dg.exe"
-	}
-
-	err := getter.GetFile(tempFile, url)
-	if err != nil {
-		return fmt.Errorf("failed to download file: %v", err)
-	}
-
-	err = os.Rename(tempFile, os.Args[0])
-	if err != nil {
-		return fmt.Errorf("failed to replace the binary: %v", err)
-	}
-
-	if runtime.GOOS != "windows" {
-		err = os.Chmod(os.Args[0], 0755)
-		if err != nil {
-			return fmt.Errorf("failed to change the file permissions: %v", err)
-		}
-	}
-
-	fmt.Println("Update successful!")
-	return nil
 }
---
+++ cmd/update.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	getter "github.com/hashicorp/go-getter"
+	"github.com/spf13/cobra"
+)
+
+var (
+	version   = "v0.1.0"
+	updateCmd *cobra.Command
+)
+
+func runUpdate(version string) error {
+	versionParam := "latest/download/dg"
+	if version != "" {
+		versionParam = fmt.Sprintf("download/%s/dg", version)
+	}
+
+	url := fmt.Sprintf("https://github.com/cognite-analytics/dbt-go/releases/%s", versionParam)
+	fmt.Printf("Updating CLI tool from %s...\n", url)
+
+	tempFile := filepath.Join(os.TempDir(), "dg")
+	if runtime.GOOS == "windows" {
+		tempFile += ".exe"
+	}
+
+	err := getter.GetFile(tempFile, url)
+	if err != nil {
+		return fmt.Errorf("failed to download file: %v", err)
+	}
+
+	targetDir := getInstallPath()
+	targetPath := filepath.Join(targetDir, "dg")
+	if runtime.GOOS == "windows" {
+		targetPath += ".exe"
+	}
+
+	err = os.Rename(tempFile, targetPath)
+	if err != nil {
+		return fmt.Errorf("failed to move binary to target directory: %v", err)
+	}
+
+	if runtime.GOOS != "windows" {
+		err = os.Chmod(targetPath, 0755)
+		if err != nil {
+			return fmt.Errorf("failed to change file permissions: %v", err)
+		}
+	}
+
+	fmt.Println("Update successful!")
+	return nil
+}
+
+func getInstallPath() string {
+	if runtime.GOOS == "windows" {
+		path, err := exec.LookPath(os.Args[0])
+		if err == nil {
+			return filepath.Dir(path)
+		}
+		return "."
+	}
+	return "/usr/local/bin"
+}
